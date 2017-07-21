ffb7580bc4eb2df17d07b87041f5c18f30d6b800
Supamiu <contact@flavien-normand.fr>
2017-07-21 21:51:22 +0200

    test: added tests for forChild configuration.

--- test/serializer.service.spec.ts
+++ test/serializer.service.spec.ts
@@ -21,6 +21,12 @@ class FooChild extends Foo {
     }
 }
 
+class FooChildTwo extends Foo {
+    public getBar(): string {
+        return 'child2-' + this.bar;
+    }
+}
+
 describe('Serializer tests', () => {
 
     beforeEach(() => {
@@ -46,4 +52,24 @@ describe('Serializer tests', () => {
         expect(service.deserialize<Foo>({discriminator: 'child', bar: 'baz'}, Foo).getBar()).to.eq('child-baz');
     }));
 
+    describe('ForChild test', () => {
+        beforeEach(() => {
+            TestBed.configureTestingModule({
+                imports: [
+                    NgSerializerModule.forChild([
+                        {
+                            parent: Foo,
+                            children: {
+                                'child2': FooChildTwo
+                            }
+                        }
+                    ])
+                ]
+            });
+        });
+
+        it('Should be able to use forChild configuration', inject([NgSerializerService], (service: NgSerializerService) => {
+            expect(service.deserialize<Foo>({discriminator: 'child2', bar: 'baz'}, Foo).getBar()).to.eq('child2-baz');
+        }));
+    });
 });
