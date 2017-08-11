7f4820f10322977af6659a2aa62e78561c0e684a
Supamiu <contact@flavien-normand.fr>
2017-08-11 13:17:24 +0200

    refactor: change concat call for a shorter one

--- src/ng-serializer.factory.ts
+++ src/ng-serializer.factory.ts
@@ -4,6 +4,6 @@ import { Registration, Registry } from '@kaiu/serializer';
 export function NgSerializerServiceFactory(registrations?: Registration[][]): NgSerializerService {
     const registry: Registry = new Registry();
     //The concat called here is used to flatten the array., allowing multi:true in provider.
-    registry.add(Array.prototype.concat.apply([], registrations));
+    registry.add([].concat(...registrations));
     return new NgSerializerService(registry);
 }
