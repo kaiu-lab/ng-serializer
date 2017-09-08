87934344231db83156073c1c953aac2508532f14
Flavien Normand <contact@flavien-normand.fr>
2017-09-08 13:59:42 +0200

    chore: changed implementation for #2 fix

--- src/ng-serializer.factory.ts
+++ src/ng-serializer.factory.ts
@@ -3,8 +3,6 @@ import { Registration, Registry } from '@kaiu/serializer';
 
 export function NgSerializerServiceFactory(registrations?: Registration[][]): NgSerializerService {
     const registry: Registry = new Registry();
-    // Because we could get undefined registrations in the registrations
-    registrations = registrations.filter(registration => registration !== undefined);
     //The concat called here is used to flatten the array, allowing multi:true in provider.
     registry.add([].concat(...registrations));
     return new NgSerializerService(registry);
